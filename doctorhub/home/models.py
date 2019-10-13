6e35b70d0b7b628b3271ad70f578df796010d439
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2019-10-13 05:14:42 +0330

    d

--- doctorhub/home/models.py
+++ doctorhub/home/models.py
@@ -96,7 +96,6 @@ class ArticlesCategoryPage(MetadataPageMixin, Page):
     ]
 
     subpage_types = [
-        'home.ArticleType1Page',
         'home.ArticlePage',
     ]
 
@@ -104,149 +103,10 @@ class ArticlesCategoryPage(MetadataPageMixin, Page):
         return self.get_parent().specific.get_home_page()
 
 
-class ArticleType1Tag(TaggedItemBase):
-    content_object = ParentalKey(
-        'ArticleType1Page', related_name='article_type1_tags', on_delete=models.CASCADE
-    )
-
-
 class ArticleTag(TaggedItemBase):
     content_object = ParentalKey(
         'ArticlePage', related_name='article_tags', on_delete=models.CASCADE
     )
-
-
-class ArticleType1Page(MetadataPageMixin, Page):
-    categories = ParentalManyToManyField(ArticleCategory, blank=False)
-    tags = ClusterTaggableManager(
-        through=ArticleType1Tag, blank=True
-    )
-    image = models.ForeignKey(
-        'wagtailimages.Image',
-        help_text='high quality image',
-        null=True, blank=False, on_delete=models.SET_NULL, related_name='+'
-    )
-    article_title = RichTextField(
-        features=[], blank=False, null=True,
-        help_text='It has to start with a farsi word'
-    )
-    article_summary = RichTextField(
-        features=configurations.RICHTEXT_FEATURES, blank=False, null=True,
-        help_text='It has to start with a farsi word'
-    )
-    article_introduction = RichTextField(
-        features=configurations.RICHTEXT_FEATURES, blank=True, null=True,
-        help_text='It has to start with a farsi word'
-    )
-    article_conclusion = RichTextField(
-        features=configurations.RICHTEXT_FEATURES, blank=True, null=True,
-        help_text='It has to start with a farsi word'
-    )
-
-    paragraphs = StreamField(
-        [
-            ('paragraph', ParagraphBlock()),
-            ('image_paragraph', ImageParagraphBlock()),
-            ('linkable_image_paragraph', LinkableImageParagraphBlock()),
-            ('linkable_paragraph', LinkableParagraphBlock()),
-            ('horizontal_image', HorizontalImageBlock()),
-            ('horizontal_image_paragraph', HorizontalImageParagraphBlock()),
-            ('list', ListBlock()),
-            ('video_paragraph', VideoParagraphBlock(icon='media')),
-        ], blank=True
-    )
-
-    article_caption = RichTextField(
-        features=[], blank=True, null=True,
-        help_text='It has to start with a farsi word'
-    )
-    is_ready = models.BooleanField(default=False)
-
-    content_panels = [
-        MultiFieldPanel(
-            [
-                RichTextFieldPanel('article_title'),
-                ImageChooserPanel('image'),
-                FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
-                FieldPanel('tags'),
-            ], heading='Details', classname="collapsible collapsed"),
-        MultiFieldPanel(
-            [
-                RichTextFieldPanel('article_summary'),
-                RichTextFieldPanel('article_introduction'),
-                StreamFieldPanel('paragraphs'),
-                RichTextFieldPanel('article_conclusion'),
-            ], heading='Paragraphs', classname="collapsible collapsed"),
-        # MultiFieldPanel(
-        #     [
-        #         RichTextFieldPanel('article_caption'),
-        #         FieldPanel('is_ready')
-        #     ], heading='Instagram', classname='collapsible collapsed'
-        # )
-    ]
-    promote_panels = []
-    settings_panels = []
-
-    api_fields = [
-        APIField('categories', serializer=ArticleCategoriesField()),
-        APIField('tags'),
-        APIField('image', serializer=ImageRenditionField(
-            'fill-2000x2000-c80|jpegquality-100')
-        ),
-        APIField('article_summary'),
-        APIField('article_introduction'),
-        APIField('article_conclusion'),
-        APIField('paragraphs', serializer=ParagraphsField()),
-    ]
-
-    @property
-    def linkable_paragraphs(self):
-        linkable_paragraphs = []
-        for paragraph in self.paragraphs:
-            block_type = paragraph.block_type
-            if 'linkable' in block_type:
-                linkable_paragraphs.append(paragraph)
-        return linkable_paragraphs
-
-    def get_home_page(self):
-        return self.get_parent().specific.get_home_page()
-
-    def clean(self):
-        super().clean()
-        if not self.id:
-            self.set_uuid4()
-            self.slug = 'type1-' + self.uuid4
-        self.title = text_processing.html_to_str(self.article_title)
-        self.search_image = self.image
-
-    def serve(self, request, *args, **kwargs):
-        self.search_description = self.title + ' شامل ' + text_processing.str_list_to_comma_separated(
-            [
-                text_processing.html_to_str(paragraph.value['title'].source)
-                for paragraph in self.linkable_paragraphs
-            ]
-        )
-        self.seo_title = 'مقالات - {} - {}'.format(
-            self.get_parent().specific.category.farsi_name,
-            self.title
-        )
-        return super().serve(request, *args, **kwargs)
-
-    uuid4 = models.TextField(default='')
-
-    def set_uuid4(self):
-        uuid4 = uuid.uuid4()
-        while ArticleType1Page.objects.filter(uuid4=uuid4).exists():
-            uuid4 = uuid.uuid4()
-        self.uuid4 = str(uuid4)
-
-    parent_page_types = ['home.ArticlesCategoryPage']
-    subpage_types = []
-
-    class Meta:
-        ordering = [
-            '-first_published_at'
-        ]
 
 
 class ArticlePage(MetadataPageMixin, Page):
@@ -300,6 +160,18 @@ class ArticlePage(MetadataPageMixin, Page):
     promote_panels = []
     settings_panels = []
 
+    # api_fields = [
+    #     APIField('categories', serializer=ArticleCategoriesField()),
+    #     APIField('tags'),
+    #     APIField('image', serializer=ImageRenditionField(
+    #         'fill-2000x2000-c80|jpegquality-100')
+    #              ),
+    #     APIField('article_summary'),
+    #     APIField('article_introduction'),
+    #     APIField('article_conclusion'),
+    #     APIField('paragraphs', serializer=ParagraphsField()),
+    # ]
+
     def get_home_page(self):
         return self.get_parent().specific.get_home_page()
 
@@ -328,7 +200,7 @@ class ArticlePage(MetadataPageMixin, Page):
 
     def set_uuid4(self):
         uuid4 = uuid.uuid4()
-        while ArticleType1Page.objects.filter(uuid4=uuid4).exists():
+        while ArticlePage.objects.filter(uuid4=uuid4).exists():
             uuid4 = uuid.uuid4()
         self.uuid4 = str(uuid4)
 
