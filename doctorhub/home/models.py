d21716f9992efbde09847e53a3569e271bd173c4
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2019-10-16 22:33:35 +0330

    d

--- doctorhub/home/models.py
+++ doctorhub/home/models.py
@@ -12,6 +12,7 @@ from wagtail.core.models import Page
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtailmetadata.models import MetadataPageMixin
 
+from .articles.webmd.models import *
 from .articles.blocks import *
 from .articles.models import *
 from .articles.serializers import *
@@ -19,7 +20,14 @@ from .modules import list_processing
 from .modules import text_processing
 
 
-class HomePage(Page):
+class DigitalTebPageMixin:
+
+    @staticmethod
+    def get_home_page():
+        return HomePage.objects.first()
+
+
+class HomePage(DigitalTebPageMixin, Page):
     subpage_types = [
         'home.ArticlesCategoriesPage'
     ]
@@ -28,7 +36,9 @@ class HomePage(Page):
         return HttpResponseRedirect('/articles/')
 
 
-class ArticlesCategoriesPage(MetadataPageMixin, Page):
+class ArticlesCategoriesPage(
+    DigitalTebPageMixin, MetadataPageMixin, Page
+):
     parent_page_types = ['home.HomePage']
     subpage_types = ['home.ArticlesCategoryPage']
 
@@ -44,9 +54,6 @@ class ArticlesCategoriesPage(MetadataPageMixin, Page):
         self.title = 'مقالات'
         self.slug = 'articles'
 
-    def get_home_page(self):
-        return self.get_parent().specific
-
     def get_row_categories(self):
         children = self.get_children().live()
         return list_processing.list_to_sublists_of_size_n(children, 2)
@@ -56,7 +63,9 @@ class ArticlesCategoriesPage(MetadataPageMixin, Page):
     settings_panels = []
 
 
-class ArticlesCategoryPage(MetadataPageMixin, Page):
+class ArticlesCategoryPage(
+    DigitalTebPageMixin, MetadataPageMixin, Page
+):
     category = models.OneToOneField(
         ArticleCategory, blank=False,
         on_delete=models.SET_NULL, null=True
@@ -96,10 +105,8 @@ class ArticlesCategoryPage(MetadataPageMixin, Page):
 
     subpage_types = [
         'home.ArticlePage',
+        'home.WebMDBlogPost',
     ]
-
-    def get_home_page(self):
-        return self.get_parent().specific.get_home_page()
 
 
 class ArticleTag(TaggedItemBase):
@@ -114,7 +121,7 @@ class ArticleEnglishTag(TaggedItemBase):
     )
 
 
-class ArticlePage(MetadataPageMixin, Page):
+class ArticlePage(DigitalTebPageMixin, MetadataPageMixin, Page):
     categories = ParentalManyToManyField(ArticleCategory, blank=False)
     tags = ClusterTaggableManager(
         through=ArticleTag, blank=True, related_name='farsi_tags'
@@ -191,9 +198,6 @@ class ArticlePage(MetadataPageMixin, Page):
     #     APIField('paragraphs', serializer=ParagraphsField()),
     # ]
 
-    def get_home_page(self):
-        return self.get_parent().specific.get_home_page()
-
     def clean(self):
         super().clean()
         if not self.id:
@@ -205,8 +209,129 @@ class ArticlePage(MetadataPageMixin, Page):
     def serve(self, request, *args, **kwargs):
         self.search_description = self.title + ' شامل ' + text_processing.str_list_to_comma_separated(
             [
-                text_processing.html_to_str(paragraph.value['title'].source)
-                for paragraph in self.sections
+                text_processing.html_to_str(section.value['title'].source)
+                for section in self.sections
+            ]
+        )
+        self.seo_title = 'مقالات - {} - {}'.format(
+            self.get_parent().specific.category.farsi_name,
+            self.title
+        )
+        return super().serve(request, *args, **kwargs)
+
+    uuid4 = models.TextField(default='')
+
+    def set_uuid4(self):
+        uuid4 = uuid.uuid4()
+        while ArticlePage.objects.filter(uuid4=uuid4).exists():
+            uuid4 = uuid.uuid4()
+        self.uuid4 = str(uuid4)
+
+    parent_page_types = ['home.ArticlesCategoryPage']
+    subpage_types = []
+
+    class Meta:
+        ordering = [
+            '-first_published_at'
+        ]
+
+
+class WebMDBlogPostFarsiTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'WebMDBlogPost', related_name='webmd_farsi_tags', on_delete=models.CASCADE
+    )
+
+
+class WebMDBlogPostEnglishTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'WebMDBlogPost', related_name='webmd_english_tags', on_delete=models.CASCADE
+    )
+
+
+class WebMDBlogPost(DigitalTebPageMixin, MetadataPageMixin, Page):
+    categories = ParentalManyToManyField(ArticleCategory, blank=False)
+    topics = ParentalManyToManyField(Topic, blank=False)
+    farsi_tags = ClusterTaggableManager(
+        through=WebMDBlogPostFarsiTag, blank=True, related_name='webmd_farsi_tags'
+    )
+    english_tags = ClusterTaggableManager(
+        through=WebMDBlogPostEnglishTag, blank=True, related_name='webmd_english_tags'
+    )
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        help_text='high quality horizontal image',
+        null=True, blank=False, on_delete=models.SET_NULL, related_name='+'
+    )
+    article_title = RichTextField(
+        features=[], blank=False, null=True,
+        help_text='It has to start with a farsi word'
+    )
+    article_summary = RichTextField(
+        features=configurations.RICHTEXT_FEATURES, blank=False, null=True,
+        help_text='It has to start with a farsi word'
+    )
+    article_introduction = RichTextField(
+        features=configurations.RICHTEXT_FEATURES, blank=True, null=True,
+        help_text='It has to start with a farsi word'
+    )
+    article_conclusion = RichTextField(
+        features=configurations.RICHTEXT_FEATURES, blank=True, null=True,
+        help_text='It has to start with a farsi word'
+    )
+    sections = StreamField(
+        [
+            ('section', SectionBlock()),
+        ], blank=True
+    )
+
+    content_panels = [
+        MultiFieldPanel(
+            [
+                RichTextFieldPanel('article_title'),
+                ImageChooserPanel('image'),
+                FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+            ], heading='Details', classname="collapsible collapsed"
+        ),
+        MultiFieldPanel(
+            [
+                RichTextFieldPanel('article_summary'),
+                RichTextFieldPanel('article_introduction'),
+                StreamFieldPanel('sections'),
+                RichTextFieldPanel('article_conclusion'),
+            ], heading='Content', classname="collapsible collapsed"
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('farsi_tags'),
+            ], heading='Farsi Tags', classname='collapsible collapsed'
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('english_tags'),
+            ], heading='English Tags', classname='collapsible collapsed'
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('topics', widget=forms.CheckboxSelectMultiple),
+            ], heading='Topics', classname="collapsible collapsed"
+        ),
+    ]
+    promote_panels = []
+    settings_panels = []
+
+    def clean(self):
+        super().clean()
+        if not self.id:
+            self.set_uuid4()
+            self.slug = 'webmd-article-' + self.uuid4
+        self.title = text_processing.html_to_str(self.article_title)
+        self.search_image = self.image
+
+    def serve(self, request, *args, **kwargs):
+        self.search_description = self.title + ' شامل ' + text_processing.str_list_to_comma_separated(
+            [
+                text_processing.html_to_str(section.value['title'].source)
+                for section in self.sections
             ]
         )
         self.seo_title = 'مقالات - {} - {}'.format(
