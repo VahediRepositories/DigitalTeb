f97edcef40e1260964a68e73075c6f984b30895e
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2019-12-10 00:00:42 +0330

    d

--- doctorhub/home/accounts/views.py
+++ doctorhub/home/accounts/views.py
@@ -329,7 +329,7 @@ class PasswordChangeView(
 def email(request):
     subject = 'Thank you for registering to our site'
     message = ' it  means a world to us '
-    email_from = settings.EMAIL_HOST_USER
+    email_from = settings.DEFAULT_FROM_EMAIL
     recipient_list = [request.GET['address'], ]
     res = send_mail(subject, message, email_from, recipient_list)
     return HttpResponse(res)
