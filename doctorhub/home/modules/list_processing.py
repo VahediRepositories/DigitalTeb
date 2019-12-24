3afe094f7107285b1c4f492075cb490cd2e6a14f
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2019-12-25 02:01:27 +0330

    d

--- doctorhub/home/modules/list_processing.py
+++ doctorhub/home/modules/list_processing.py
@@ -1,2 +1,5 @@
 def list_to_sublists_of_size_n(any_list=[], n=1):
-    return [any_list[i * n:(i + 1) * n] for i in range((len(any_list) + n - 1) // n)]
+    l = [any_list[i * n:(i + 1) * n] for i in range((len(any_list) + n - 1) // n)]
+    for item in l:
+        item.reverse()
+    return l
